e40c1e87ee78db7fd3bbd03f08c5ccb0663e958d
kkdroidgit <kartikdroid17@gmail.com>
2020-12-19 18:52:52 +0530

    added react-router-dom

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+    return (
+        <>
+        <nav class="navbar">
+          <div class="navbar-container">
+            
+          </div>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
