dd9d211995118459ac4292e61c5052a3edd710d2
kkdroidgit <kartikdroid17@gmail.com>
2020-12-19 19:11:54 +0530

    cdn fontawesome

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
+import {Link} from 'react-router-dom';
 
 function Navbar() {
     return (
         <>
         <nav class="navbar">
           <div class="navbar-container">
-            
+            <Link to="/" className="navbar-logo">
+              TRVL <i class="fab fa-typo3"/>
+            </Link>
           </div>
         </nav>
         </>
